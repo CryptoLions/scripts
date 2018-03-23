93de4344e3f848bc3ce219c1f17e51e0fd7376ef
ansigroup <37701692+ansigroup@users.noreply.github.com>
2018-03-23 17:51:57 +0200

    Update archive_blocks.sh

--- archive_blocks.sh
+++ archive_blocks.sh
@@ -3,13 +3,16 @@
 #  
 # Scrip Created by http://CryptoLions.io  
 #  
+# Change PATH_TO_ARCH to place where you woud like to store archives
+# Change networkName to your blockchain network name
 ###############################################################################  
 
 PATH_TO_ARCH="."
+BLOCKCHAIN_NAME="networkName"
 DATE=`date -d "now" +'%Y_%m_%d-%H_%M'`
 
-echo "Archiving Blocks [$DATE]"
+echo "Archiving Blocks $BLOCKCHAIN_NAME [$DATE]"
 
 ./stop.sh
-tar cvf $PATH_TO_ARCH/blocks-$DATE.tar blocks
+tar cvf $PATH_TO_ARCH/blocks-$BLOCKCHAIN_NAME-$DATE.tar blocks
 ./start.sh
