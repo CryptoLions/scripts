ec10e960d6b1fe4e440322687a70f86da3def4bb
ansigroup <37701692+ansigroup@users.noreply.github.com>
2018-03-23 16:26:20 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+EOSIOBINDIR=/path/to/eos/bin
+DATADIR=/path/to/data-dir
+
+$EOSIOBINDIR/eosiod --replay --enable-stale-production true --data-dir $DATADIR  > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/eosd.pid
