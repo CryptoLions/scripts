bee41b565fabadb53b0bf4a8fa67668faac09658
ansigroup <37701692+ansigroup@users.noreply.github.com>
2018-03-24 20:00:35 +0200

    Update archive_blocks.sh

--- archive_blocks.sh
+++ archive_blocks.sh
@@ -7,12 +7,13 @@
 # Change networkName to your blockchain network name
 ###############################################################################  
 
-PATH_TO_ARCH="."
+PATH_TO_ARCH="/var/www/html/blocks"
 BLOCKCHAIN_NAME="networkName"
 DATE=`date -d "now" +'%Y_%m_%d-%H_%M'`
 
 echo "Archiving Blocks $BLOCKCHAIN_NAME [$DATE]"
 
 ./stop.sh
-tar -pcvzf $PATH_TO_ARCH/blocks-$BLOCKCHAIN_NAME-$DATE.tar.gz blocks
+tar -pcvzf $PATH_TO_ARCH/blocks-$BLOCKCHAIN_NAME-$DATE.tar.gz blocks  
+ln -sf $PATH_TO_ARCH/blocks-$BLOCKCHAIN_NAME-$DATE.tar.gz $PATH_TO_ARCH/blocks.tar.gz  
 ./start.sh
