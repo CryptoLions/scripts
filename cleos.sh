40efdbea48b1de244392ee57e153e08a2049156e
ansigroup <37701692+ansigroup@users.noreply.github.com>
2018-04-07 17:16:00 +0300

    Update cleos.sh

--- cleos.sh
+++ cleos.sh
@@ -8,7 +8,7 @@
 
 EOSIOBINDIR=/home/eos-dawn-v3.0.0/eos/build/programs
 WALLETHOST="127.0.0.1"
-NODEPORT="8887"
-WALLETPORT="8887"
+NODEPORT="8888"
+WALLETPORT="8888"
 
 $EOSIOBINDIR/cleos/cleos -p $NODEPORT --wallet-host $WALLETHOST --wallet-port $WALLETPORT $@
