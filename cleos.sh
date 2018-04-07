259e8e23aceb4fc7d0b7a2a4c4e68efaca8caf33
ansigroup <37701692+ansigroup@users.noreply.github.com>
2018-04-07 15:02:34 +0300

    Create cleos.sh

---
+++ cleos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+################################################################################
+#
+# Scrip Created by http://CryptoLions.io
+# https://github.com/CryptoLions/scripts/
+#
+###############################################################################
+
+EOSIOBINDIR=/home/eos-dawn-v3.0.0/eos/build/programs
+WALLETHOST="127.0.0.1"
+WALLETPORT="8888"
+
+$EOSIOBINDIR/cleos/cleos --wallet-host $WALLETHOST --wallet-port $WALLETPORT $@
