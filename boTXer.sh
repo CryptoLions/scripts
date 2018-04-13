ee0f100bc3e6452391d08b46abe44532aa330452
ansigroup <37701692+ansigroup@users.noreply.github.com>
2018-04-13 18:04:43 +0300

    Update boTXer.sh

--- boTXer.sh
+++ boTXer.sh
@@ -25,7 +25,7 @@ WALLETPORT="8905"
 SENDERS=("volcano" "rain" "magic" "surprise" "lottery")
 RECEIVERS=("lion" "tiger" "jaguar" "bat" "mowgli" "dragonfly" "elephant2" "mosquito" "wombat" "fox" "gorilla" "honeybadger" "sloth" "langurs" "tokki" "whale" "panther" "tortoise" "galapago" "mpenjati" "cougars" "ladybird" "giraffe" "rhino" "cheetah" "termite" "snake" "tapir" "boar" "spider" "koala" "beaver" "unicorn" "scorpion" "hummingbird" "kangaroo" "dragon" "macaw" "parrot" "pug")
 STRINGS=("&#9752;Jungle Bot" "&#9737; Be Free" "&#9736; Test Tx" "&#9733; rnd txt" "&#9732;" "&#9748; Rainforest" "&#9813;Treasure" "&#9760;Bot =)" "&#9762; bot tx" "&#9730; your umbrella?" "&#9728;" "&#9749; cofee" "&#9981; refuel")
-TX_INTERVAL=2
+TX_INTERVAL=3
 CLEOS="$EOSIOBINDIR/cleos/cleos -p $NODEPORT --wallet-host $WALLETHOST --wallet-port $WALLETPORT"
 
 CONTRACT="token"
