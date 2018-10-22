31252912af068048b636f56d485418e38d22c7cb
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-10-22 10:19:45 +0300

    Update archive_blocks.sh

--- archive_blocks.sh
+++ archive_blocks.sh
@@ -18,4 +18,8 @@ echo "Archiving BlockChain $BLOCKCHAIN_NAME [$DATE]"
 $DATA_DIR/stop.sh
 tar -pcvzf $PATH_FOR_ARCH/blocks-$BLOCKCHAIN_NAME-$DATE.tar.gz $DATA_DIR/blocks
 ln -sf $PATH_FOR_ARCH/blocks-$BLOCKCHAIN_NAME-$DATE.tar.gz $PATH_FOR_ARCH/blocks.tar.gz
+
+tar -pcvzf $PATH_FOR_ARCH/state-$BLOCKCHAIN_NAME-$DATE.tar.gz $DATA_DIR/state
+ln -sf $PATH_FOR_ARCH/state-$BLOCKCHAIN_NAME-$DATE.tar.gz $PATH_FOR_ARCH/state.tar.gz
+
 $DATA_DIR/start.sh
