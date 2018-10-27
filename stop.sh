16750908fb47db60dd4a890b12da5f65ddd4d1fb
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-10-27 12:01:49 +0300

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -22,6 +22,12 @@ DIR="/path/to/data-dir"
             echo -ne "."
             sleep 1
         done
+        DATE=$(date -d "now" +'%Y_%m_%d-%H_%M')
+        if [ ! -d $DIR/logs ]; then
+            mkdir $DIR/logs
+        fi
+        tar -pcvzf $DIR/logs/stderr-$DATE.txt.tar.gz stderr.txt stdout.txt
+
         echo -ne "\rNode Stopped.    \n"
 
     fi
