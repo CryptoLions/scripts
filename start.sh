ee731c72c5b2f05c9b25643729e587589e936284
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-12-14 15:27:03 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,10 +7,10 @@
 ###############################################################################
 
 
-EOSIOBINDIR=/home/eos-dawn-v3.0.0/eos/build/programs
+EOSIOBINDIR=/opt/eos/bin
 DATADIR=/path/to/data-dir
 
 $EOSIOBINDIR/nodes/nodes  --data-dir $DATADIR  > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/eosd.pid
 
-#$EOSIOBINDIR/nodes/nodes --replay --data-dir $DATADIR  > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/eosd.pid
-#$EOSIOBINDIR/nodes/nodes --enable-stale-production true --data-dir $DATADIR  > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/eosd.pid
+#$EOSIOBINDIR/nodes --replay --data-dir $DATADIR  > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/eosd.pid
+#$EOSIOBINDIR/nodes --enable-stale-production true --data-dir $DATADIR  > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/eosd.pid
